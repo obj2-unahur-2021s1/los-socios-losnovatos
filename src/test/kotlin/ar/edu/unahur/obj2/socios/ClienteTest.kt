461ccf4fcd95179e396c65c9a7d10b0a7cc6e6eb
JulianDiani <juliandiani95@gmail.com>
2021-05-11 19:08:14 -0300

    casi terminado,faltaria algun test mas

--- src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
@@ -1,8 +1,23 @@
 package ar.edu.unahur.obj2.socios
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 
 class ClienteTest : DescribeSpec({
-  describe("Un/a cliente") {
+  describe("cliente resfriado de barrio las lauchas da 500 de propina a un pedido de 1000") {
+    val pepe=Cliente(100.0,LasLauchas,Resfriado)
+    pepe.propinaTotalADar(1000.0).shouldBe(500.0)
+  }
+  describe("cliente enojado de las lauchas no da propina"){
+    val pepa=Cliente(500000.0,LasLauchas,Enojado)
+    pepa.propinaTotalADar(5000.0).shouldBe(0)
+  }
+  describe("cliente enojado de barrio las rosas da 50 pesos de propina"){
+    val pepinha=Cliente(125.0,LasRosas,Enojado)
+    pepinha.propinaTotalADar(50.0).shouldBe(50)
+  }
+  describe("cliente feliz de barrio verde da 200 de propina a un pedido menor a 200"){
+    val pipo=Cliente(125.0,BarrioVerde,Feliz)
+    pipo.propinaTotalADar(125.0).shouldBe(200)
   }
 })
--- src/test/kotlin/ar/edu/unahur/obj2/socios/repl.ws.kts
+++ src/test/kotlin/ar/edu/unahur/obj2/socios/repl.ws.kts
@@ -1,7 +1,13 @@
+import ar.edu.unahur.obj2.socios.Cliente
+import ar.edu.unahur.obj2.socios.Enojado
+import ar.edu.unahur.obj2.socios.LasRosas
+
 // Pueden usar este archivo para hacer pruebas rápidas,
 // de la misma forma en que usaban el REPL de Wollok.
 
 // OJO: lo que esté aquí no será tenido en cuenta
 // en la corrección ni reemplaza a los tests.
 
-listOf(1, 8, 10).average()
+val pepe=Cliente(1200.0,LasRosas,Enojado)
+pepe.dineroEnBolsillo
+
