7dbc70e03eebb361439eb4a5500f1153b406debd
leozeballez <zeballezleonel@hotmail.com>
2021-05-11 23:59:53 -0300

    agregar tests restantes

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -3,38 +3,39 @@ package ar.edu.unahur.obj2.socios
 import kotlin.math.max
 
 abstract class EstadoDeAnimo{
-   abstract fun calcularPropina(cliente: Cliente,montoPedido:Double):Double
+   abstract fun calcularPropina(cliente: Cliente, montoPedido:Double): Double
 }
-object Enojado :EstadoDeAnimo(){
-    override fun calcularPropina(cliente: Cliente,montoPedido:Double)=0.0
+object Enojado: EstadoDeAnimo(){
+    override fun calcularPropina(cliente: Cliente,montoPedido:Double)= 0.0
 }
-object Feliz:EstadoDeAnimo(){
-    override fun calcularPropina(cliente: Cliente,montoPedido: Double)=montoPedido*0.25
+object Feliz: EstadoDeAnimo(){
+    override fun calcularPropina(cliente: Cliente,montoPedido: Double)= montoPedido*0.25
 }
-object Indiferente:EstadoDeAnimo(){
-    override fun calcularPropina(cliente: Cliente,montoPedido: Double)=cliente.dineroEnBolsillo
+object Indiferente: EstadoDeAnimo(){
+    override fun calcularPropina(cliente: Cliente,montoPedido: Double)= cliente.dineroEnBolsillo
 }
-object Resfriado:EstadoDeAnimo(){
-    override fun calcularPropina(cliente: Cliente,montoPedido: Double)=montoPedido
+object Resfriado: EstadoDeAnimo(){
+    override fun calcularPropina(cliente: Cliente,montoPedido: Double)= montoPedido
 }
-class Cliente(val dineroEnBolsillo:Double,var barrioEnElQueVive:Barrio,var estadoDeAnimo:EstadoDeAnimo){
-    fun propinaSegunEstadoDeAnimo(montoPedido:Double)=estadoDeAnimo.calcularPropina(this,montoPedido)
-    fun propinaTotalADar(montoPedido: Double)=barrioEnElQueVive.calcularPropina(this,montoPedido)
+class Cliente(var dineroEnBolsillo: Double, var barrioEnElQueVive: Barrio, var estadoDeAnimo: EstadoDeAnimo) {
+
+    fun propinaSegunEstadoDeAnimo(montoPedido:Double)= estadoDeAnimo.calcularPropina(this, montoPedido)
+    fun propinaSegunBarrio(montoPedido: Double)= barrioEnElQueVive.calcularPropina(this, montoPedido)
 
 }
-abstract class Barrio{
-    abstract fun calcularPropina(cliente: Cliente,montoPedido: Double):Double
+abstract class Barrio {
+    abstract fun calcularPropina(cliente: Cliente, montoPedido: Double): Double
 }
-object LasRosas:Barrio(){
-    override fun calcularPropina(cliente: Cliente, montoPedido: Double)=cliente.propinaSegunEstadoDeAnimo(montoPedido)+50
+object LasRosas: Barrio() {
+    override fun calcularPropina(cliente: Cliente, montoPedido: Double)= cliente.propinaSegunEstadoDeAnimo(montoPedido) + 50
 }
-object LasLauchas:Barrio(){
-    override fun calcularPropina(cliente: Cliente,montoPedido: Double)=cliente.propinaSegunEstadoDeAnimo(montoPedido)/2
+object LasLauchas: Barrio() {
+    override fun calcularPropina(cliente: Cliente, montoPedido: Double)= cliente.propinaSegunEstadoDeAnimo(montoPedido) /2
 }
-object BarrioVerde:Barrio(){
-    override fun calcularPropina(cliente: Cliente, montoPedido: Double)=max(cliente.propinaSegunEstadoDeAnimo(montoPedido),200.0)
+object BarrioVerde: Barrio() {
+    override fun calcularPropina(cliente: Cliente, montoPedido: Double)= max(cliente.propinaSegunEstadoDeAnimo(montoPedido),200.0)
 }
-object LasTorres:Barrio(){
-    override fun calcularPropina(cliente: Cliente, montoPedido: Double)=cliente.propinaSegunEstadoDeAnimo(montoPedido)
+object LasTorres: Barrio() {
+    override fun calcularPropina(cliente: Cliente, montoPedido: Double)= cliente.propinaSegunEstadoDeAnimo(montoPedido)
 }
 
--- src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
@@ -3,21 +3,30 @@ package ar.edu.unahur.obj2.socios
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
-class ClienteTest : DescribeSpec({
-  describe("cliente resfriado de barrio las lauchas da 500 de propina a un pedido de 1000") {
-    val pepe=Cliente(100.0,LasLauchas,Resfriado)
-    pepe.propinaTotalADar(1000.0).shouldBe(500.0)
+class ClienteTest: DescribeSpec ({
+
+  describe("Un cliente resfriado de barrio las lauchas da $500 de propina a un pedido de $1000") {
+    val pepe = Cliente(100.0, LasLauchas, Resfriado)
+    pepe.propinaSegunBarrio(1000.0).shouldBe(500.0)
   }
-  describe("cliente enojado de las lauchas no da propina"){
-    val pepa=Cliente(500000.0,LasLauchas,Enojado)
-    pepa.propinaTotalADar(5000.0).shouldBe(0)
+  describe("Un cliente enojado de lasLauchas no da propina"){
+    val pepa = Cliente(500000.0, LasLauchas, Enojado)
+    pepa.propinaSegunBarrio(5000.0).shouldBe(0)
   }
-  describe("cliente enojado de barrio las rosas da 50 pesos de propina"){
-    val pepinha=Cliente(125.0,LasRosas,Enojado)
-    pepinha.propinaTotalADar(50.0).shouldBe(50)
+  describe("Un cliente enojado de barrio lasRosas da $50 pesos de propina"){
+    val pepinha = Cliente(125.0, LasRosas, Enojado)
+    pepinha.propinaSegunBarrio(50.0).shouldBe(50)
   }
-  describe("cliente feliz de barrio verde da 200 de propina a un pedido menor a 200"){
-    val pipo=Cliente(125.0,BarrioVerde,Feliz)
-    pipo.propinaTotalADar(125.0).shouldBe(200)
+  describe("Un cliente feliz de barrio verde da $200 de propina a un pedido menor a $200"){
+    val pipo = Cliente(125.0, BarrioVerde, Feliz)
+    pipo.propinaSegunBarrio(125.0).shouldBe(200)
+  }
+  describe("Un cliente feliz de barrioVerde da $ de propina a un pedido mayor a $200"){
+    val pipo = Cliente(125.0, BarrioVerde, Feliz)
+    pipo.propinaSegunBarrio(125.0).shouldBe(200)
+  }
+  describe("Un cliente indiferente de lasTorres da $30 de propina a un pedido de $800"){
+    val pipo = Cliente(30.0, LasTorres, Indiferente)
+    pipo.propinaSegunBarrio(800.0).shouldBe(30)
   }
 })
